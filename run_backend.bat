d1dc6632b36affa489794ec6c8812193dd2d0ca0
Redder <redder.va@gmail.com>
2026-01-09 21:39:04 +0200

    Clean up: remove unused files (batch scripts, tests, old docs)

--- run_backend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Run backend Flask API
-"%~dp0venv311\Scripts\python.exe" main.py
-pause
--- run_frontend.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d "%~dp0frontend"
-node "C:\Program Files\nodejs\node_modules\npm\bin\npm-cli.js" start
